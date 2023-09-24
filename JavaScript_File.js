446dec2e86d858852b899c1fadb89819525d7ca2
Pabloskito <139324193+Pabloskito@users.noreply.github.com>
2023-09-24 20:09:45 +0200

    JavaScript_File.js

--- JavaScript_File.js
+++ JavaScript_File.js
@@ -1,11 +1,1 @@
-this.load.image('Gato', 'Images\R (13).png');
-var personaje = {
-    sprite: 'Gato',
-    x: 100,
-    y: 100
-};
-var cursors;
-cursors = this.input.keyboard.createCursorKeys();
-if (cursors.up.isDown) {
-    personaje.y -= 2;
-}
+
